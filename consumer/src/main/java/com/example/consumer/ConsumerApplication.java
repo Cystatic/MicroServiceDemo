dd708919652348a95d6dc70c05a3cb330b138224
caosy <csy015676@163.com>
2022-03-10 14:16:53 +0800

    Initial commit

---
+++ consumer/src/main/java/com/example/consumer/ConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.example.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@ImportResource("consumer.xml")
+@SpringBootApplication
+public class ConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+}
